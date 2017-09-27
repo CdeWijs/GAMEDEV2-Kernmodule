794542391500de92734ef12a575d5ddfd7020682
CdeWijs <31764854+CdeWijs@users.noreply.github.com>
2017-09-27 21:22:02 +0200

    Add files via upload

---
+++ Snake Unity Project/Assets/Scripts/CandySpawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandySpawn : MonoBehaviour {
+
+    public GameObject candyPrefab;
+
+    public Transform borderUp;
+    public Transform borderDown;
+    public Transform borderLeft;
+    public Transform borderRight;
+
+    public void Spawn() {
+        int x = (int)Random.Range(borderLeft.position.x, borderRight.position.x);
+        int y = (int)Random.Range(borderDown.position.y, borderUp.position.y);
+
+        Instantiate(candyPrefab, new Vector2(x, y), Quaternion.identity);
+    }
+}
